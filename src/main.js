2261e2ad6f8bba4212f4f030ddbf738cb9c50861
ethan-choshsh <ethan.s.cho@polarissharetech.com>
2022-11-04 17:48:17 +0900

    Init

---
+++ src/main.js
@@ -0,0 +1,34 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+export function run() {
+  // input
+  const branch = core.getInput('branch');
+  const token = core.getInput('token');
+
+  // var
+  const allowPrefixList = ['feature/', 'fix/', 'seo/', 'refactor/', 'hotfix/', 'ci/', 'dependabot/'];
+  const check = false;
+
+  for (allowPrefix in allowPrefixList) {
+    if (branch.startsWith(allowPrefix)) {
+      check = true;
+      break;
+    }
+  }
+
+  if (!check) {
+    const octokit = github.getOctokit(token);
+    const issue = github.context.issue();
+    const comment = `PR Title must be started with: ${allowPrefixList}`;
+
+    octokit.rest.issues.createComment({
+      owner: issue.owner,
+      repo: issue.repo,
+      issue_number: issue.number,
+      body: comment,
+    });
+
+    core.setFailed(comment);
+  }
+}
