0f7d998f834b2e9da0f08a5afcb8a20c578804ae
ethan-choshsh <ethan.s.cho@polarissharetech.com>
2023-04-06 16:59:27 +0900

    refactor: 코드 정리

--- dist/index.js
+++ dist/index.js
@@ -9703,14 +9703,19 @@ __nccwpck_require__.r(__webpack_exports__);
 const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 
+/**
+ * Main function to run the GitHub Action.
+ */
 function run() {
-  // input
   const token = core.getInput('token');
+  const { context } = github;
+  const { payload, issue, actor } = context;
+  const { pull_request: pullRequest, repository } = payload;
+  const { name: repositoryName } = repository;
 
-  // var
-  const head = github.context.payload.pull_request.head.ref.toLowerCase();
-  const base = github.context.payload.pull_request.base.ref.toLowerCase();
-  const allowPrefixList = [
+  const headBranch = pullRequest.head.ref.toLowerCase();
+  const baseBranch = pullRequest.base.ref.toLowerCase();
+  const allowedPrefixes = [
     'feat/',
     'refactor/',
     'fix/',
@@ -9722,42 +9727,50 @@ function run() {
     'revert-',
   ];
 
-  if (github.context.payload.repository.name === 'polarishare-frontend-2022') {
-    allowPrefixList.push('seo/', 'test/');
+  if (repositoryName === 'polarishare-frontend-2022') {
+    allowedPrefixes.push('seo/', 'test/');
   }
 
-  let check = false;
+  const isValidBranch = checkBranchName(headBranch, baseBranch, allowedPrefixes);
 
-  if (base !== 'master') {
-    check = true;
-  } else {
-    for (let i = 0; i < allowPrefixList.length; i++) {
-      if (head.startsWith(allowPrefixList[i])) {
-        console.log('check:', head.startsWith(allowPrefixList[i]), 'branch:', head, 'prefix:', allowPrefixList[i]);
-        check = true;
-        break;
-      }
-    }
-  }
-
-  if (!check) {
+  if (!isValidBranch) {
     const octokit = github.getOctokit(token);
-    const issue = github.context.issue;
-
-    let comment = `### Branch Name must be started with:`;
-    allowPrefixList.forEach((allowPrefix) => {
-      comment += `\n- ${allowPrefix}`;
-    });
+    const commentBody = createCommentBody(allowedPrefixes);
 
     octokit.rest.issues.createComment({
       owner: issue.owner,
       repo: issue.repo,
       issue_number: issue.number,
-      body: `@${github.context.actor}\n${comment}`,
+      body: `@${actor}\n${commentBody}`,
     });
 
-    core.setFailed(comment);
+    core.setFailed(commentBody);
   }
+}
+
+/**
+ * Check if branch name starts with any of the allowed prefixes.
+ * @param {string} headBranch - Head branch name.
+ * @param {string} baseBranch - Base branch name.
+ * @param {Array<string>} allowedPrefixes - List of allowed branch name prefixes.
+ * @returns {boolean} - Returns true if the branch name is valid, otherwise false.
+ */
+function checkBranchName(headBranch, baseBranch, allowedPrefixes) {
+  if (baseBranch !== 'master') {
+    return true;
+  }
+
+  return allowedPrefixes.some((prefix) => headBranch.startsWith(prefix));
+}
+
+/**
+ * Generate the comment body for an invalid branch name.
+ * @param {Array<string>} allowedPrefixes - List of allowed branch name prefixes.
+ * @returns {string} - Returns the generated comment body.
+ */
+function createCommentBody(allowedPrefixes) {
+  const commentIntro = '### Branch Name must be started with:';
+  return allowedPrefixes.reduce((acc, prefix) => acc + `\n- ${prefix}`, commentIntro);
 }
 
 ;// CONCATENATED MODULE: ./src/index.js
--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+/**
+ * Main function to run the GitHub Action.
+ */
 export function run() {
-  // input
   const token = core.getInput('token');
+  const { context } = github;
+  const { payload, issue, actor } = context;
+  const { pull_request: pullRequest, repository } = payload;
+  const { name: repositoryName } = repository;
 
-  // var
-  const head = github.context.payload.pull_request.head.ref.toLowerCase();
-  const base = github.context.payload.pull_request.base.ref.toLowerCase();
-  const allowPrefixList = [
+  const headBranch = pullRequest.head.ref.toLowerCase();
+  const baseBranch = pullRequest.base.ref.toLowerCase();
+  const allowedPrefixes = [
     'feat/',
     'refactor/',
     'fix/',
@@ -20,40 +25,48 @@ export function run() {
     'revert-',
   ];
 
-  if (github.context.payload.repository.name === 'polarishare-frontend-2022') {
-    allowPrefixList.push('seo/', 'test/');
+  if (repositoryName === 'polarishare-frontend-2022') {
+    allowedPrefixes.push('seo/', 'test/');
   }
 
-  let check = false;
+  const isValidBranch = checkBranchName(headBranch, baseBranch, allowedPrefixes);
 
-  if (base !== 'master') {
-    check = true;
-  } else {
-    for (let i = 0; i < allowPrefixList.length; i++) {
-      if (head.startsWith(allowPrefixList[i])) {
-        console.log('check:', head.startsWith(allowPrefixList[i]), 'branch:', head, 'prefix:', allowPrefixList[i]);
-        check = true;
-        break;
-      }
-    }
-  }
-
-  if (!check) {
+  if (!isValidBranch) {
     const octokit = github.getOctokit(token);
-    const issue = github.context.issue;
-
-    let comment = `### Branch Name must be started with:`;
-    allowPrefixList.forEach((allowPrefix) => {
-      comment += `\n- ${allowPrefix}`;
-    });
+    const commentBody = createCommentBody(allowedPrefixes);
 
     octokit.rest.issues.createComment({
       owner: issue.owner,
       repo: issue.repo,
       issue_number: issue.number,
-      body: `@${github.context.actor}\n${comment}`,
+      body: `@${actor}\n${commentBody}`,
     });
 
-    core.setFailed(comment);
+    core.setFailed(commentBody);
   }
+}
+
+/**
+ * Check if branch name starts with any of the allowed prefixes.
+ * @param {string} headBranch - Head branch name.
+ * @param {string} baseBranch - Base branch name.
+ * @param {Array<string>} allowedPrefixes - List of allowed branch name prefixes.
+ * @returns {boolean} - Returns true if the branch name is valid, otherwise false.
+ */
+function checkBranchName(headBranch, baseBranch, allowedPrefixes) {
+  if (baseBranch !== 'master') {
+    return true;
+  }
+
+  return allowedPrefixes.some((prefix) => headBranch.startsWith(prefix));
+}
+
+/**
+ * Generate the comment body for an invalid branch name.
+ * @param {Array<string>} allowedPrefixes - List of allowed branch name prefixes.
+ * @returns {string} - Returns the generated comment body.
+ */
+function createCommentBody(allowedPrefixes) {
+  const commentIntro = '### Branch Name must be started with:';
+  return allowedPrefixes.reduce((acc, prefix) => acc + `\n- ${prefix}`, commentIntro);
 }
